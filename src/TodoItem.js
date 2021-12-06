b59e3ac15af01901977cfc928fd9b9464cbdf3ec
Jorge Alexander Taberoa Jimenez <taberoajorge@pm.me>
2021-12-06 13:21:13 -0600

    Se corrigen estilizado

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,8 +4,10 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="todo-item" >
-        <input type="checkbox" />
-        <p>{props.text}</p>
+        
+        <input  type="checkbox" id="checkbox"/>
+        <label  id="label" for="checkbox">{props.text}</label>
+        
         <button className="button-delete">
         <svg  xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
         </button>
